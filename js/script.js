1309b2a13826b7841611e176388c93c1486f8b81
Evgeniy <evgeniy261105@yandex.ru>
2022-01-03 11:58:15 +0400

    Методы и свойства строк и чисел

--- js/script.js
+++ js/script.js
@@ -1,21 +1,125 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
+// Циклы
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+/* if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+} */
+
+/* const num = 50;
+
+ if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error'); */
+
+
+/* const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно!');
+        break;
+    case 100: 
+        console.log('Неверно!');
+        break;
+    case 50: 
+        console.log('Верно!');
+        break;
+    default:
+        console.log('Не в этот раз :(');
+        break; 
+}   */
+
+
+//let num = 50;
+
+/* while (num <= 55) {
+    console.log(num);
+    num++;
+} */
+
+/* for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+} */
+
+
+
+// Функции 
+
+/* let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num);
+}
+
+showFirstMessage('Hello world!');
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log('Hello!');
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+logger();
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+const bebebe = (a,b) => { 
+    return a + b 
+}; */
 
-console.log(personalMovieDB); 
+
+// Методы и свойства строк и чисел
+
+/* const str = 'test';
+const arr = [1, 2, 4];
+
+console.log(arr.length); */
+
+const str = 'test';
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = 'Some       fruit';
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello world';
+
+// console.log(logg.slice(6, 11));
+// console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+//console.log(parseInt(test));
+console.log(parseFloat(test));
